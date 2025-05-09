1fdb329eb2d52657ed2e4c55189a90fbd69eccbf
trest <trestcontra@bk.ru>
2025-05-09 18:52:13 +0600

    KS-1-5-hour-minute-second

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,31 @@
 package org.example.lesson_1
 
+import jdk.internal.net.http.common.Log
+
 fun main(){
 
-    var seconds: String = "6480"
+    var seconds = 6480
     println("Время в космосе в секундах -- $seconds секунд")
 
-    var minute = 108
+    val minute = (seconds % 3600/60)
     println("Время в космосе в минутах -- $minute минут")
 
-    val hour: String = "01"
-    minute = 40
-    seconds = "00"
+    val hour = (seconds/3600)
+    println("Время в космосе в часах -- $hour часов")
+
+    seconds = seconds %60
+    println("Секунды не вошедшие в минуту -- $seconds секунд")
 
     println("Время проведенное космонавтом в открытом космосе -- $hour:$minute:$seconds")
+
+//    var minute = { seconds: 60 ->
+//    }
+//    println("Время в космосе в минутах -- $minute минут")
+//
+//    val hour: String = "01"
+//    minute = 40
+//    seconds = "00"
+//
+//    println("Время проведенное космонавтом в открытом космосе -- $hour:$minute:$seconds")
 
 }
