867b22734ef86492573ab5b2ed2d3c214f390b30
trest <trestcontra@bk.ru>
2025-05-10 19:46:30 +0600

    KS-2-2_correction

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,20 +1,20 @@
 package org.example.lesson_2
 
-fun  main(){
+fun main() {
 
     val numberOfWorkers = 50
     val numberOfInterns = 30
     val salaryWorkers = 30000
     val salaryInterns = 20000
 
-    val workerCosts = numberOfWorkers*salaryWorkers
+    val workerCosts = numberOfWorkers * salaryWorkers
     println("Расходы на рабочих -- $workerCosts рублей")
 
-    val internsCosts = numberOfInterns*salaryInterns
-    val costs = internsCosts+workerCosts
+    val internsCosts = numberOfInterns * salaryInterns
+    val costs = internsCosts + workerCosts
     println("Общие расходы после прихода интернов -- $costs рублей")
 
-    val averageSalary = (costs/(numberOfInterns+numberOfWorkers))
+    val averageSalary = (costs / (numberOfInterns + numberOfWorkers))
     println("Средняя зарплата сотрудников после прихода стажеров -- $averageSalary рублей")
 
 }
