e4167d9709046eeb6ac06780eb2cced4b9745997
trest <trestcontra@bk.ru>
2025-05-11 19:41:40 +0600

    KS-1-5_correction5

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,23 +1,18 @@
 package org.example.lesson_1
 
-
 fun main() {
-
     val seconds = 6480
-
     val minutes = (seconds / SIXTY) - SIXTY
-
     val hour = (seconds / SIXTY) / SIXTY
-
     val remainingSeconds = (seconds % SIXTY)
 
     println(
-        "Время проведенное космонавтом в открытом космосе -- " +
-                "${"%02d".format(hour)}:" +
-                "${"%02d".format(minutes)}:" +
-                "%02d".format(remainingSeconds)
+        "Время проведенное космонавтом в открытом космосе -- %02d:%02d:%02d".format(
+            hour,
+            minutes,
+            remainingSeconds
+        )
     )
-
 }
 
 const val SIXTY = 60
