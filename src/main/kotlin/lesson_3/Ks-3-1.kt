f73f2260234df14a0fe98717ee1e4aea8cd4ead7
trest <trestcontra@bk.ru>
2025-05-12 12:40:50 +0600

    Ks-3-1 morning evening

---
+++ src/main/kotlin/lesson_3/Ks-3-1.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_3
+
+import kotlin.random.Random
+
+fun main() {
+
+    val name = "Mark"
+    val morning = "Good morning!"
+    val evening = "Good evening!"
+
+    val morningEvening = arrayOf(morning, evening)
+    val random: Int = Random.nextInt(1, 3)
+    val morningOrEvening = morningEvening[random - 1]
+    val greetings = if (morningOrEvening == morning) {
+        "$morning $name"
+    } else {
+        "$evening $name"
+    }
+
+    println(greetings)
+}
