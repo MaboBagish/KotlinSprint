19a4573f63e4cc0673a5461cf465eef4c6e06bcf
trest <trestcontra@bk.ru>
2025-05-12 15:06:22 +0600

    fix2

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -12,5 +12,4 @@ fun main() {
     val averageScore: Double = ((studentPoint1 + studentPoint2 + studentPoint3 + studentPoint4) / numberOfStudents).toDouble()
 
     println("Средний балл по английскому в этом классе -- ${"%.2f".format(averageScore)}")
-
 }
