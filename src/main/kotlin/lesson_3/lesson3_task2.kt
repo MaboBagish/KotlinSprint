e6ceaef743418b3d1735b77c76e3911541be64bb
trest <trestcontra@bk.ru>
2025-05-13 17:21:42 +0600

    fix

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -2,15 +2,18 @@ package org.example.lesson_3
 
 fun main() {
 
-    val name = "Татьяна"
-    val patronymic = "Сергеевна"
-    val surname20 = "Андреева"
-    val surname22 = "Сидорова"
-    val old20 = 20
-    val old22 = 22
+    var name = "Татьяна Сергеевна Андреевна"
+    var old = 20
 
     println(
-        "$surname20 $name $patronymic, $old20 лет\n" +
-                "$surname22 $name $patronymic, $old22 года"
+        "$name  $old лет"
     )
+
+    name = "Татьяна Сергеевна Сидорова"
+    old = 22
+
+    println(
+        "$name  $old года"
+    )
+
 }
