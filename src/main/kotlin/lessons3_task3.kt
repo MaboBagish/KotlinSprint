4beb2e04e09cdd508d75daf77973cd119b07cc12
trest <trestcontra@bk.ru>
2025-05-13 17:45:38 +0600

    fix

--- src/main/kotlin/lessons3_task3.kt
+++ src/main/kotlin/lessons3_task3.kt
@@ -4,12 +4,27 @@ fun main() {
 
     println("Таблица умножения. Введите число которое надо умножить")
     val numberToMultiply: Int = readln().toInt()
-    var i: Int = 0
 
-    while (i <= 9) {
-        i++
-        var result = i * numberToMultiply
-        println("$numberToMultiply * $i = $result")
-    }
+    val one = 1
+    val two = 2
+    val three = 3
+    val four = 4
+    val five = 5
+    val six = 6
+    val seven = 7
+    val eight = 8
+    val nine = 9
+
+    println(
+        "$numberToMultiply * $one = ${numberToMultiply * one}\n" +
+                "$numberToMultiply * $two = ${numberToMultiply * two}\n" +
+                "$numberToMultiply * $three = ${numberToMultiply * three}\n" +
+                "$numberToMultiply * $four = ${numberToMultiply * four}\n" +
+                "$numberToMultiply * $five = ${numberToMultiply * five}\n" +
+                "$numberToMultiply * $six = ${numberToMultiply * six}\n" +
+                "$numberToMultiply * $seven = ${numberToMultiply * seven}\n" +
+                "$numberToMultiply * $eight = ${numberToMultiply * eight}\n" +
+                "$numberToMultiply * $nine = ${numberToMultiply * nine}"
+    )
 
 }
