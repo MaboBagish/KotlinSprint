a12c39c7dc059e309c1dd05c0d3b7a6cad62b62b
trest <trestcontra@bk.ru>
2025-05-13 18:21:00 +0600

    Ks-3-4 Chess

---
+++ src/main/kotlin/lesson_3/Ks-3-4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var startingPosition = "e2"
+    var finalPosition = "e4"
+
+    println("Первый ход. Пешка $startingPosition на $finalPosition")
+
+    startingPosition = "d2"
+    finalPosition = "d3"
+
+    println("Второй ход. Пешка $startingPosition на $finalPosition")
+
+}
