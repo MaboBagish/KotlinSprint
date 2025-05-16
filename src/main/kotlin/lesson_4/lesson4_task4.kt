0d2e845cbdfe39f463cde3eb98b105459ded44de
trest <trestcontra@bk.ru>
2025-05-16 16:33:24 +0600

    fix

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -5,9 +5,9 @@ fun main() {
     val day = 5
     val typeOfTraining = if (day % 2 == 0) false else true
 
-
     println("Упражнение для рук: ${if(typeOfTraining == true) true else false}\n" +
             "Упражнение для ног: ${if(typeOfTraining == true) false else true}\n" +
             "Упражнение для спины: ${if(typeOfTraining == true) false else true}\n" +
             "Упражнение для пресса: ${if (typeOfTraining == true) true else false}\n")
+
 }
