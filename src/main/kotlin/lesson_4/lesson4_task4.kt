94da6e1878be73d851de4c079372588ba2b459d1
trest <trestcontra@bk.ru>
2025-05-16 16:57:23 +0600

    fix

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,11 +3,12 @@ package org.example.lesson_4
 fun main() {
 
     val day = 5
-    val typeOfTraining = if (day % 2 == 0) false else true
 
-    println("Упражнение для рук: ${if(typeOfTraining == true) true else false}\n" +
-            "Упражнение для ног: ${if(typeOfTraining == true) false else true}\n" +
-            "Упражнение для спины: ${if(typeOfTraining == true) false else true}\n" +
-            "Упражнение для пресса: ${if (typeOfTraining == true) true else false}\n")
+    println(
+        "Упражнение для рук: ${if (day % 2 != 0) true else false}\n" +
+        "Упражнение для ног: ${if (day % 2 != 0) false else true}\n" +
+        "Упражнение для спины: ${if (day % 2 != 0) false else true}\n" +
+        "Упражнение для пресса: ${if (day % 2 != 0) true else false}\n"
+    )
 
 }
