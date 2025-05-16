25e568861756d2f6eee74e05a1a32689e4a88e66
trest <trestcontra@bk.ru>
2025-05-16 14:38:29 +0600

    fix

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -5,10 +5,22 @@ fun main() {
     val sunWeather = true
     val openAwning = true
     val airHumidity = 20
-    val сurrentTimeOfYear = "WINTER"
+    val сurrentTimeOfWinter = false
 
-    println("Благоприятные ли условия сейчас для роста бобовый ? " +
-            "${if (airHumidity == 20 && sunWeather == true && openAwning == true && сurrentTimeOfYear == "WINTER") true 
-            else  false}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовый ? " +
+                "${
+                    if (airHumidity == HUMIDITY_20_PERCENT && 
+                        sunWeather == PRESENCE_OF_SUNNY_WEATHER &&
+                        openAwning == THE_TENT_IS_OPEN && 
+                        сurrentTimeOfWinter == CURRENT_SEASON_WINTER) true
+                    else false
+                }"
+    )
 
 }
+
+const val PRESENCE_OF_SUNNY_WEATHER = true
+const val THE_TENT_IS_OPEN = true
+const val HUMIDITY_20_PERCENT = 20
+const val CURRENT_SEASON_WINTER = false
