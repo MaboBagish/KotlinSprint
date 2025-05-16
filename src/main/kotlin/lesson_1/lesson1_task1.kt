8e6019561a9dd9d0d39dca4d07dafa9c9f70c50e
trest <trestcontra@bk.ru>
2025-05-16 11:03:03 +0600

    Ks1-1

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main(){
+
+    val numberOfOrders: Int = 75
+    val thankYouPurchase: String = "Спасибо за покупку"
+
+    println("Количество заказов $numberOfOrders штук")
+    println(thankYouPurchase)
+
+}
