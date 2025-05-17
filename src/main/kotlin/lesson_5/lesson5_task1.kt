31e929505a4d8f635873207dce058d883e1c6328
trest <trestcontra@bk.ru>
2025-05-17 14:15:09 +0600

    Fix

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_5
 
+import kotlin.random.Random
+
 fun main() {
 
     println("Для входа в приложение Вам нужно будет ввести два числа при сложении которых результат будет 5")
     println("Введите первое число")
-    val a = readln().toInt()
+    val a = Random.nextInt()
     println("Введите второе число")
-    val b = readln().toInt()
+    val b = Random.nextInt()
 
-    val c = a + b
 
-   val resultat = if (c == 5) "Добро пожаловать" else "Доступ запрещен"
+    val resultat = if ((a + b) == 5) "Добро пожаловать" else "Доступ запрещен"
 
     println(resultat)
 }
